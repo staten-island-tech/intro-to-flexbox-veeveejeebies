046ea2518a7d79a05465f653a9150d8729fa3414
veeveejeebies <vavinz@nycstudents.net>
2025-10-08 12:28:09 -0400

    dddd

---
+++ index.js
@@ -0,0 +1,13 @@
+const plushies = [
+  {
+    name: "Hachiware",
+    price: 39.99,
+    inStock: true,
+    img: "img/hachiware.webp",
+    alt: "hachiware plushie"
+  }
+];
+
+plushies.forEach((plushie) => {
+  console.log(plushie.name);
+});
